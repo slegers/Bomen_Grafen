00a2e285163a07e53315fd9f82a6c1928d3f5c94
Yanice Slegers <yanice@dumbledore.home>
2017-06-11 16:58:52 +0200

    Binary tree fully impl

--- Bin_tree/src/main/java/domain/BinaryTree.java
+++ Bin_tree/src/main/java/domain/BinaryTree.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class BinaryTree<E> {
 	private E data;
 	private BinaryTree<E> leftTree, rightTree;
@@ -19,5 +21,74 @@ public class BinaryTree<E> {
 			if (this.leftTree != null) this.leftTree.printPreorder();
 			if (this.rightTree != null) this.rightTree.printPreorder();
 	}
+	public void printInOrder(){
+		if (this.leftTree != null) this.leftTree.printInOrder();
+		System.out.print(this.data + " ");
+		if (this.rightTree != null) this.rightTree.printInOrder();
+	}
+	public void printPostOrder(){
+		if (this.leftTree != null) this.leftTree.printPostOrder();
+		if (this.rightTree != null) this.rightTree.printPostOrder();
+		System.out.print(this.data + " ");
+	}
+	public int countNodes(){
+		if(leftTree == null){
+			if(rightTree == null){
+				return 1;
+			}else{
+				return 1+ rightTree.countNodes();
+			}
+		}else{
+			if(rightTree == null){
+				return 1+ leftTree.countNodes();
+			}else{
+				return 1 + leftTree.countNodes() + rightTree.countNodes();
+			}
+		}
+		/******************
+		 * Simple version
+		 *****************/
+		/*
+		return 1 + (this.leftTree == null ? 0 : this.leftTree.countNodes())
+		+ (this.rightTree == null ? 0 : this.rightTree.countNodes());
+		 */
+	}
+	public int getDepth() {
+		return 1 + Math.max((leftTree == null ? 0 : leftTree.getDepth()), (rightTree == null ? 0 : rightTree.getDepth()));
+	}
+	public boolean isLeaf() {
+		return leftTree == null && rightTree == null;
+	}
+	public int countLeaves() {
+		if (this.isLeaf()) {
+			return 1;
+		} else {
+			return (this.leftTree == null ? 0 : this.leftTree.countLeaves())
+				+ (this.rightTree == null ? 0 : this.rightTree.countLeaves());
+	}
 
+
+	}
+	public ArrayList<E> getDataLeaves() {
+		ArrayList<E> res = new ArrayList<>();
+		if (this.isLeaf()) {
+			res.add(this.data);
+		} else {
+			res = (this.leftTree == null ? new ArrayList<>() : this.leftTree.getDataLeaves());
+			ArrayList<E> rightLeaves = (this.rightTree == null ? new ArrayList<>() : this.rightTree.getDataLeaves()); res.addAll(rightLeaves);
+		}
+		return res;
+	}
+	public boolean contains(E s) {
+
+		if(s == null){
+			return false;
+		}else{
+			if(s.equals(data)){
+				return true;
+			}else{
+				return leftTree != null && leftTree.contains(s) ? true : rightTree != null && rightTree.contains(s);
+			}
+		}
+	}
 }
--- Bin_tree/src/main/java/ui/BinaryTreeDriver.java
+++ Bin_tree/src/main/java/ui/BinaryTreeDriver.java
@@ -2,6 +2,8 @@ package ui;
 
 import domain.BinaryTree;
 
+import java.util.ArrayList;
+
 public class BinaryTreeDriver {
 
 	public static void main(String[] args) {
@@ -19,7 +21,25 @@ public class BinaryTreeDriver {
 		
 		// boom heeft root C en heeft links A en rechts G
 		BinaryTree<String> boom = new BinaryTree<>("C",nodeA, nodeG);
+		System.out.println("Pre order");
 		boom.printPreorder();
+		System.out.println("In order");
+		boom.printInOrder();
+		System.out.println("Post order");
+		boom.printPostOrder();
+		System.out.println();
+		System.out.print("counts: " + boom.countNodes());
+		System.out.println();
+		System.out.print("depth: " + boom.getDepth());
+		System.out.println();
+		System.out.print("leafs: " + boom.countLeaves());
+		System.out.println();
+		System.out.print("dataleafs: " + boom.getDataLeaves());
+		System.out.println();
+		System.out.print("dataleafs: " + boom.getDataLeaves());
+		System.out.println();
+		System.out.print("dataleafs: " + boom.contains("Z"));
 	}
+
 
 }
